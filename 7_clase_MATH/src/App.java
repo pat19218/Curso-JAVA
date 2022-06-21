559e173a1a14cf3d7b971262692bb4141451d1c2
Cris Pat <68085020+pat19218@users.noreply.github.com>
2022-06-21 11:31:20 -0600

    .

--- 7_clase_MATH/src/App.java
+++ 7_clase_MATH/src/App.java
@@ -1,5 +1,28 @@
+/*
+ * Autor: Cristhofer Patzan
+ * Tema: clase MATH
+ * 
+ */
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        double raiz = Math.sqrt(9); //solo funciona con double
+        System.out.println("El resultado es: " + raiz);
+        //si quiero mayor espacio en memoria y estoy seguro que la raiz es entera entonces lo indexo
+        //int raiz1 = (int) Math.sqrt(9);
+
+        double base = 5;
+        double exponente = 2;
+        double potencia = Math.pow(base, exponente);
+        System.out.println("El resultado es: " + potencia);
+
+        double numero = 4.56;
+        long resultado = Math.round(numero); //la función round tiene dos opciones una para redondear de tipo double a long
+                                               //y otra para redondear de float a int
+        System.out.println("El resultado es: " + resultado);
+
+        numero = Math.random();
+        System.out.println("El resultado es: " + numero);
     }
 }
