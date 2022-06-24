a10b8696ff6bc1518384ee93968a77c738a17cad
Cris Pat <68085020+pat19218@users.noreply.github.com>
2022-06-24 14:51:06 -0600

    q

--- 10_condicionales/src/App.java
+++ 10_condicionales/src/App.java
@@ -13,10 +13,32 @@ public class App {
 
         numero = Integer.parseInt(JOptionPane.showInputDialog("Ingresa un dato"));
 
+
+        //---------------------------------------------------------------------------------------------------------------------------------------
+        //      IF, ELSE, ELSE IF
+        //---------------------------------------------------------------------------------------------------------------------------------------
+
+
+        /*
+         *  == : Igual a
+         *  != : Distinto de
+         *  <  : menor que
+         *  >  : mayor que
+         *  <= : menor o igual que
+         *  >= : mayor o igual que
+         * 
+         */
+
         if (numero == dato){
             
             JOptionPane.showMessageDialog(null, "Has acertado al numero secreto! ", "Juego de numero",
             JOptionPane.PLAIN_MESSAGE);
+
+        }else if (numero == 4 || numero == 6){
+
+            JOptionPane.showMessageDialog(null, "Has estado cerca", "Juego de numero",
+            JOptionPane.PLAIN_MESSAGE);
+
 
         }else{
 
