0d57f5412d49cb520e717b299b2121ba5ed18e3e
Cris Pat <68085020+pat19218@users.noreply.github.com>
2022-07-01 15:45:54 -0600

    ok

--- 15_matriz/src/App.java
+++ 15_matriz/src/App.java
@@ -1,5 +1,63 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+/*
+ * Author: Cris Pat
+ * Tema:   Matrices
+ * 
+ */
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        
+        //-------------------------------------------------------------------------------------------------------------------------------
+        //   
+        //-------------------------------------------------------------------------------------------------------------------------------
+
+        int matriz [][] = { {1, 2, 3},
+                            {4, 5, 6},
+                            {7, 8, 9}};
+        int nFilas, nColumnas;
+
+        for(int i=0; i<3; i++){         //numero de filas
+            for(int j=0; j<3; j++){     //numero de columnas
+                
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+
+        //------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------
+        //------------------------------------------------------------------------------------------------------------------------------
+        
+        Scanner entrada = new Scanner(System.in);
+
+        nFilas = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el numero de filas"));
+        nColumnas = Integer.parseInt(JOptionPane.showInputDialog("Ingresa el numero de columnas"));
+
+        matriz = new int[nFilas][nColumnas];
+
+        for(int i=0; i<nFilas; i++){         //numero de filas
+            for(int j=0; j<nColumnas; j++){     //numero de columnas
+                
+                System.out.print("Llena la matriz en [" + i + "]" +" [" + j + "] "  );
+                matriz[i][j] = entrada.nextInt();
+                
+            }
+            
+        }
+
+        System.out.println("\n la matriz es \n");
+
+        for(int i=0; i<3; i++){         //numero de filas
+            for(int j=0; j<3; j++){     //numero de columnas
+                
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+
     }
 }
